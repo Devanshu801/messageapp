60ee663671d9971cb45017c8cbfc0ff8e9cd7f1c
Devanshu801 <52850353+Devanshu801@users.noreply.github.com>
2021-01-20 08:26:37 -0800

    Create Status.java

---
+++ Status.java
@@ -0,0 +1,13 @@
+package message;
+
+import java.util.ArrayList;
+
+public class Status {
+ boolean name;
+ ArrayList<User> SeenUser;
+ Message message;
+public static void setStatus(Message m) {
+	// TODO Auto-generated method stub
+	
+} 
+}
