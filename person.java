1cc8dbe7852a6930b705be233cea67a8acaa5640
Devanshu801 <52850353+Devanshu801@users.noreply.github.com>
2021-01-20 08:21:48 -0800

    Create person.java

---
+++ person.java
@@ -0,0 +1,19 @@
+package message;
+
+public class person {
+  String name;
+
+public person(String name) {
+	super();
+	this.name = name;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+  
+}
