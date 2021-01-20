695384c43d397b3b17b448902ad102888549c54c
Devanshu801 <52850353+Devanshu801@users.noreply.github.com>
2021-01-20 08:23:10 -0800

    Create Dm.java

---
+++ Dm.java
@@ -0,0 +1,14 @@
+package message;
+
+import java.util.ArrayList;
+
+public class Dm {
+
+ArrayList<Message> send;
+ArrayList<Message> Receive;
+public Dm(ArrayList<Message> send, ArrayList<Message> receive) {
+	super();
+	this.send = send;
+	Receive = receive;
+}
+}
